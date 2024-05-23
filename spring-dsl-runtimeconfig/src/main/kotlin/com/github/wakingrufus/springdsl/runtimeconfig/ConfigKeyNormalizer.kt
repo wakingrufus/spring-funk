2f88b5d81754496dd2339e0b28731502077f6799
wakingrufus <wakingrufus@gmail.com>
2024-05-23 18:08:37 -0500

    enhance runtime config features

---
+++ spring-dsl-runtimeconfig/src/main/kotlin/com/github/wakingrufus/springdsl/runtimeconfig/ConfigKeyNormalizer.kt
@@ -0,0 +1,37 @@
+package com.github.wakingrufus.springdsl.runtimeconfig
+
+fun normalizeConfigKey(rawKey: String): String {
+    return translateLowerCaseWithSeparator(rawKey.replace("_", "-"), '-')
+}
+
+private fun translateLowerCaseWithSeparator(input: String, separator: Char): String {
+    val length = input.length
+    if (length == 0) {
+        return input
+    }
+
+    val result = StringBuilder(length + (length shr 1))
+    var upperCount = 0
+    for (i in 0 until length) {
+        val ch = input[i]
+        val lc = ch.lowercaseChar()
+
+        if (lc == ch) { // lower-case letter means we can get new word
+            // but need to check for multi-letter upper-case (acronym), where assumption
+            // is that the last upper-case char is start of a new word
+            if (upperCount > 1) {
+                // so insert hyphen before the last character now
+                result.insert(result.length - 1, separator)
+            }
+            upperCount = 0
+        } else {
+            // Otherwise starts new word, unless beginning of string
+            if ((upperCount == 0) && (i > 0)) {
+                result.append(separator)
+            }
+            ++upperCount
+        }
+        result.append(lc)
+    }
+    return result.toString()
+}
--- spring-dsl-runtimeconfig/src/main/kotlin/com/github/wakingrufus/springdsl/runtimeconfig/RuntimeConfigDslInitializer.kt
+++ spring-dsl-runtimeconfig/src/main/kotlin/com/github/wakingrufus/springdsl/runtimeconfig/RuntimeConfigDslInitializer.kt
@@ -2,49 +2,54 @@ package com.github.wakingrufus.springdsl.runtimeconfig
 
 import com.github.wakingrufus.springdsl.base.getDsl
 import io.github.oshai.kotlinlogging.KotlinLogging
-import org.springframework.beans.factory.support.RootBeanDefinition
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.cloud.context.environment.EnvironmentChangeEvent
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ApplicationListener
 import org.springframework.context.support.GenericApplicationContext
-import org.springframework.core.ResolvableType
-import java.util.function.Supplier
 
 /**
  * Spring implementation of {@link RuntimeConfigDsl}
  */
 class RuntimeConfigDslInitializer : ApplicationContextInitializer<GenericApplicationContext> {
-    private val log = KotlinLogging.logger {}
     override fun initialize(applicationContext: GenericApplicationContext) {
         applicationContext.getDsl<RuntimeConfigDsl>()?.apply {
             configClasses.forEach { configClassEntry ->
-                log.info { configClassEntry.toString() }
-                val beanDefinition = RootBeanDefinition(RuntimeConfig::class.java)
-                beanDefinition.setTargetType(ResolvableType.forClassWithGenerics(RuntimeConfig::class.java, configClassEntry.clazz))
-                beanDefinition.instanceSupplier = Supplier {
-                    SpringRuntimeConfig.create(
-                        configClassEntry.clazz,
-                        applicationContext.environment,
-                        configClassEntry.prefix
-                    )
-                }
-                applicationContext.registerBeanDefinition(configClassEntry.clazz.name, beanDefinition)
+                registerRuntimeConfig(
+                    applicationContext,
+                    configClassEntry.clazz,
+                    configClassEntry.prefix
+                )
             }
-            applicationContext.addApplicationListener((ApplicationListener<ApplicationReadyEvent> {
-                log.info { "initializing RuntimeConfigs" }
-                val runtimeConfigs = applicationContext.getBeansOfType(SpringRuntimeConfig::class.java)
-                runtimeConfigs.forEach { (_, u) -> u.update() }
-            }))
-            applicationContext.addApplicationListener((ApplicationListener<EnvironmentChangeEvent> { event ->
-                log.info { "propagating runtime config updates to RuntimeConfigs" }
-                val runtimeConfigs = applicationContext.getBeansOfType(SpringRuntimeConfig::class.java)
-                runtimeConfigs.forEach { (_, obj) ->
-                    if (event.keys.any { it.startsWith(obj.prefix) }) {
-                        obj.update()
-                    }
+            applicationContext.addApplicationListener(Initializer(applicationContext))
+            applicationContext.addApplicationListener(Updater(applicationContext))
+        }
+    }
+
+
+    private class Initializer(val applicationContext: GenericApplicationContext) :
+        ApplicationListener<ApplicationReadyEvent> {
+        private val log = KotlinLogging.logger {}
+        override fun onApplicationEvent(event: ApplicationReadyEvent) {
+            log.info { "initializing RuntimeConfigs" }
+            val runtimeConfigs = applicationContext.getBeansOfType(SpringRuntimeConfig::class.java)
+            runtimeConfigs.forEach { (_, obj) ->
+                obj.update()
+            }
+        }
+    }
+
+    private class Updater(val applicationContext: GenericApplicationContext) :
+        ApplicationListener<EnvironmentChangeEvent> {
+        private val log = KotlinLogging.logger {}
+        override fun onApplicationEvent(event: EnvironmentChangeEvent) {
+            log.info { "propagating runtime config updates to RuntimeConfigs" }
+            val runtimeConfigs = applicationContext.getBeansOfType(SpringRuntimeConfig::class.java)
+            runtimeConfigs.forEach { (_, obj) ->
+                if (event.keys.stream().anyMatch { it.startsWith(obj.prefix) }) {
+                    obj.update()
                 }
-            }))
+            }
         }
     }
 }
