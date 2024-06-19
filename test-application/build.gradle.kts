09e9e0c7922f537848a3d9ab506b8d3bd71f932b
John Burns <wakingrufus@users.noreply.github.com>
2024-06-19 17:01:35 -0500

    fix package naming in example app (#35)
    add example app integration test

--- test-application/build.gradle.kts
+++ test-application/build.gradle.kts
@@ -2,15 +2,25 @@ plugins {
     application
     id("org.springframework.boot") version ("3.+")
     kotlin("jvm")
+    `jvm-test-suite`
 }
 repositories {
     mavenCentral()
 }
 dependencies{
     implementation(project(":spring-funk-webmvc"))
-    implementation("org.springframework.boot:spring-boot-starter-jetty:3.2.5")
+    implementation(spring.boot.jetty)
     implementation("ch.qos.logback:logback-classic:1.5.6")
 
+    testImplementation(spring.boot.test)
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+    }
 }
 
 java {
---
+++ test-application/src/test/kotlin/com/github/wakingrufus/funk/example/ExampleApplicationTest.kt
@@ -0,0 +1,22 @@
+package com.github.wakingrufus.funk.example
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import java.net.URI
+
+@SpringBootTest(classes = [ExampleApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ExampleApplicationTest {
+
+    @Autowired
+    lateinit var client: TestRestTemplate
+
+    @Test
+    fun test() {
+        val response = client.getForEntity<String>(URI.create("/dsl"))
+        assertThat(response.statusCode.value()).isEqualTo(200)
+    }
+}
