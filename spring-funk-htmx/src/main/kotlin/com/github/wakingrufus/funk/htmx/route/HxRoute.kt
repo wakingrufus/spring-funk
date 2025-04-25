066035efe34e297dd7cb3432804bc61703fb3056
John Burns <wakingrufus@users.noreply.github.com>
2025-04-25 14:53:49 -0500

    support HTMX GET routes with no params (#78)

---
+++ spring-funk-htmx/src/main/kotlin/com/github/wakingrufus/funk/htmx/route/HxRoute.kt
@@ -0,0 +1,33 @@
+package com.github.wakingrufus.funk.htmx.route
+
+import kotlinx.html.TagConsumer
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.web.servlet.function.RouterFunctionDsl
+import org.springframework.web.servlet.function.ServerRequest
+import org.springframework.web.servlet.function.ServerResponse
+import org.w3c.dom.Document
+
+interface HxRoute {
+    fun registerRoutes(beanFactory: BeanFactory, dsl: RouterFunctionDsl)
+}
+
+fun <CONTROLLER : Any, REQ : Record, RESP : Record> withParam(
+    routerFunction: RouterFunctionDsl.(String, (ServerRequest) -> ServerResponse) -> Unit,
+    path: String,
+    requestClass: Class<REQ>,
+    controllerClass: Class<CONTROLLER>,
+    binding: CONTROLLER.(REQ) -> RESP,
+    renderer: (RESP) -> TagConsumer<Document>.() -> Document
+): HxRoute {
+    return ParamRoute(routerFunction, path, requestClass, controllerClass, binding, renderer)
+}
+
+fun <CONTROLLER : Any, RESP : Record> noParam(
+    routerFunction: RouterFunctionDsl.(String, (ServerRequest) -> ServerResponse) -> Unit,
+    path: String,
+    controllerClass: Class<CONTROLLER>,
+    binding: CONTROLLER.() -> RESP,
+    renderer: (RESP) -> TagConsumer<Document>.() -> Document
+): HxRoute {
+    return NoParamRoute(routerFunction, path, controllerClass, binding, renderer)
+}
---
+++ spring-funk-htmx/src/test/kotlin/com/github/wakingrufus/funk/htmx/HtmxPageTest.kt
@@ -0,0 +1,51 @@
+package com.github.wakingrufus.funk.htmx
+
+import kotlinx.html.span
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class HtmxPageTest {
+    @JvmRecord
+    data class TestRequestClass(val id: UUID)
+
+    @JvmRecord
+    data class TestResponseClass(val id: UUID)
+    class TestController {
+        fun noParamGet(): TestResponseClass {
+            return TestResponseClass(UUID.randomUUID())
+        }
+
+        fun get(req: TestRequestClass): TestResponseClass {
+            return TestResponseClass(req.id)
+        }
+    }
+
+    @Test
+    fun `test no param get`() {
+        val htmxPage = HtmxPage("").apply {
+            get("", TestController::noParamGet) {
+                {
+                    span {
+                        +it.id.toString()
+                    }
+                }
+            }
+        }
+        assertThat(htmxPage.routes).hasSize(1)
+    }
+
+    @Test
+    fun `test get with params`() {
+        val htmxPage = HtmxPage("").apply {
+            route(HttpVerb.GET, "", TestController::get) {
+                {
+                    span {
+                        +it.id.toString()
+                    }
+                }
+            }
+        }
+        assertThat(htmxPage.routes).hasSize(1)
+    }
+}
